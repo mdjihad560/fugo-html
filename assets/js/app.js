a99cd59bf3800845ac300692ccd1bf999e064b4d
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-09-22 16:35:20 +0600

    update home

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 (function ($) {
   "use strict";
   /*------------------------------------------------------------------
@@ -346,6 +348,232 @@
         type: 'iframe',
         mainClass: 'mfp-fade'
       });
+    } // fugo old versions
+
+    /*--------------------------------------------------------------
+    COUNTER JS INIT
+    --------------------------------------------------------------*/
+
+
+    var nexto_counter = $('#fugu--counter');
+
+    if (nexto_counter.is_exist()) {
+      var a = 0;
+      var oTop = $(nexto_counter).offset().top - window.innerHeight;
+
+      if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.fugu--counter').each(function () {
+          var $this = $(this),
+              countTo = $this.attr('data-percentage');
+          $({
+            countNum: $this.text()
+          }).animate({
+            countNum: countTo
+          }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function step() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function complete() {
+              $this.text(this.countNum);
+            }
+          });
+        });
+        a = 1;
+      }
+    }
+    /*--------------------------------------------------------------
+    COUNTER TWO JS INIT
+    --------------------------------------------------------------*/
+
+
+    var nexto_counter = $('#fugu--counter2');
+
+    if (nexto_counter.is_exist()) {
+      var a = 0;
+      $(window).scroll(function () {
+        var oTop = $(nexto_counter).offset().top - window.innerHeight;
+
+        if (a == 0 && $(window).scrollTop() > oTop) {
+          $('.fugu--counter').each(function () {
+            var $this = $(this),
+                countTo = $this.attr('data-percentage');
+            $({
+              countNum: $this.text()
+            }).animate({
+              countNum: countTo
+            }, {
+              duration: 4000,
+              easing: 'swing',
+              step: function step() {
+                $this.text(Math.floor(this.countNum));
+              },
+              complete: function complete() {
+                $this.text(this.countNum);
+              }
+            });
+          });
+          a = 1;
+        }
+      });
+    }
+    /*--------------------------------------------------------------
+    NEXTO SLIDER ONE
+    --------------------------------------------------------------*/
+
+
+    var nexto_slider_one = $('.fugu--slider-one');
+
+    if (nexto_slider_one.is_exist()) {
+      var _nexto_slider_one$sli;
+
+      nexto_slider_one.slick((_nexto_slider_one$sli = {
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: false
+      }, _defineProperty(_nexto_slider_one$sli, "autoplay", true), _defineProperty(_nexto_slider_one$sli, "autoplaySpeed", 2000), _defineProperty(_nexto_slider_one$sli, "prevArrow", '<button class="slide-arrow prev-arrow"></button>'), _defineProperty(_nexto_slider_one$sli, "nextArrow", '<button class="slide-arrow next-arrow"></button>'), _defineProperty(_nexto_slider_one$sli, "responsive", [{
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }]), _nexto_slider_one$sli));
+    }
+    /*--------------------------------------------------------------
+    NEXTO SLIDER TWO
+    --------------------------------------------------------------*/
+
+
+    var nexto_slider_two = $('.fugu--slider-two');
+
+    if (nexto_slider_two.is_exist()) {
+      nexto_slider_two.slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 10000,
+        cssEase: 'linear',
+        pauseOnHover: true,
+        adaptiveHeight: true,
+        responsive: [{
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true
+          }
+        }, {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }, {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+          }
+        }]
+      });
+    }
+    /*--------------------------------------------------------------
+    NEXTO TEXT SLIDER
+    --------------------------------------------------------------*/
+
+
+    var nexto_text_slider = $('.fugu--text-slider');
+
+    if (nexto_text_slider.is_exist()) {
+      nexto_text_slider.slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 10000,
+        cssEase: 'linear',
+        pauseOnHover: true,
+        adaptiveHeight: true,
+        responsive: [{
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+      });
+    }
+    /*--------------------------------------------------------------
+    SCROLL EFFECT ONE JS INIT
+    ------------------------------------------------------------*/
+
+
+    var $rotateOne = $('#rotateOne');
+    var $win = $(window);
+    $win.on('scroll', function () {
+      var right = 3 - $win.scrollTop() * 0.01;
+      $rotateOne.css('transform', 'rotate(' + right + 'deg)');
+    });
+    /*--------------------------------------------------------------
+    NEXTO SWIPER SLIDER JS INIT
+    ------------------------------------------------------------*/
+
+    var swiper_slider = $('.fugu--swiper-slider');
+
+    if (swiper_slider.is_exist()) {
+      swiper_slider = new Swiper(".fugu--swiper-slider", {
+        loop: true,
+        effect: "cards",
+        grabCursor: true,
+        autoplay: true
+      });
+    }
+    /*--------------------------------------------------------------
+    CARD LIKE JS INIT
+    ------------------------------------------------------------*/
+
+
+    var nextoBtnTigger = document.querySelectorAll('.fugu--tigger');
+    nextoBtnTigger.forEach(function (linkbtn) {
+      linkbtn.addEventListener('click', function () {
+        linkbtn.classList.toggle("dark-btn");
+      });
+    });
+    /*--------------------------------------------------------------
+    fugu MAGNIFIC POPUP JS INIT
+    ------------------------------------------------------------*/
+
+    var popup_youtube = $('.fugu--popup');
+
+    if (popup_youtube.is_exist()) {
+      popup_youtube.magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade'
+      });
     }
   });
   /*End document ready*/
@@ -563,6 +791,86 @@
       isotope();
       $(window).resize(isotope);
       var $optionSets = $('.fugu-gallery-menu .option-set'),
+          $optionLinks = $optionSets.find('li');
+      $optionLinks.click(function () {
+        var $this = $(this);
+        var $optionSet = $this.parents('.option-set');
+        $optionSet.find('.active').removeClass('active');
+        $this.addClass('active'); // make option object dynamically, i.e. { filter: '.my-filter-class' }
+
+        var options = {},
+            key = $optionSet.attr('data-option-key'),
+            value = $this.attr('data-option-value'); // parse 'false' as false boolean
+
+        value = value === 'false' ? false : value;
+        options[key] = value;
+
+        if (key === 'layoutMode' && typeof changeLayoutMode === 'function') {
+          // changes in layout modes need extra logic
+          changeLayoutMode($this, options);
+        } else {
+          // creativewise, apply new options
+          $container.isotope(options);
+        }
+
+        return false;
+      });
+    } // fugu old 
+
+    /*--------------------------------------------------------------
+    FOUR COLUMN FILTER JS INIT
+    ------------------------------------------------------------*/
+
+
+    var nexto_filter = $('#fugu--four-column');
+
+    if (nexto_filter.is_exist()) {
+      var $container = $(nexto_filter),
+          colWidth = function colWidth() {
+        var w = $container.width(),
+            columnNum = 1,
+            columnWidth = 0;
+
+        if (w > 1200) {
+          columnNum = 4;
+        } else if (w > 900) {
+          columnNum = 3;
+        } else if (w > 600) {
+          columnNum = 2;
+        } else if (w > 450) {
+          columnNum = 1;
+        } else if (w > 385) {
+          columnNum = 1;
+        }
+
+        columnWidth = Math.floor(w / columnNum);
+        $container.find('.collection-grid-item').each(function () {
+          var $item = $(this),
+              multiplier_w = $item.attr('class').match(/collection-grid-item-w(\d)/),
+              multiplier_h = $item.attr('class').match(/collection-grid-item-h(\d)/),
+              width = multiplier_w ? columnWidth * multiplier_w[1] : columnWidth,
+              height = multiplier_h ? columnWidth * multiplier_h[1] * 0.4 - 12 : columnWidth * 0.5;
+          $item.css({
+            width: width //height: height
+
+          });
+        });
+        return columnWidth;
+      },
+          isotope = function isotope() {
+        $container.isotope({
+          resizable: false,
+          itemSelector: '.collection-grid-item',
+          masonry: {
+            columnWidth: colWidth(),
+            gutterWidth: 0
+          }
+        });
+      };
+
+      isotope();
+      $(window).resize(isotope);
+      var $optionSets = $('.fugu--portfolio-menu .option-set'),
           $optionLinks = $optionSets.find('li');
       $optionLinks.click(function () {
         var $this = $(this);
